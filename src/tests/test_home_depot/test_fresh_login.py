9b5db9126541f744a2142c4545c202f257984449
Yassine Issoufou <68094236+fulanii@users.noreply.github.com>
2023-10-14 21:01:17 -0500

    started writing test for fresh_login

--- src/tests/test_home_depot/test_fresh_login.py
+++ src/tests/test_home_depot/test_fresh_login.py
@@ -3,7 +3,9 @@ from src.bot.home_depot.logins import FreshLogin
 
 class TestLogin:
     def test_fresh_login(self):
-        ...
+        email = ""
+        password = ""
+        fres_login = FreshLogin(email=email, password=password, headless=True, quit_driver=True)
 
     def test_cookie_logout(self):
         ...
